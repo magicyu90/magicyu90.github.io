a581973f68fdf11f43f87f558d2e5895cedd55c5
magicyu90 <magicyu90@gmail.com>
2019-11-17 23:23:23 +0800

    Site updated: 2019-11-17 23:23:20

--- dist/music.js
+++ dist/music.js
@@ -10,7 +10,7 @@ const ap = new APlayer({
       {
           name: '沙滩恋曲',
           artist: '头文字D 电影原声',
-          url: 'https://music.163.com/outchain/player?type=2&id=27591663&auto=1&height=66',
+          url: 'http://m10.music.126.net/20191117234723/72d070f25fb5720679088f37266555fa/ymusic/e303/dfbe/913c/f3628c355995d4a76158060fdd8280ac.mp3',
           cover: 'https://p2.music.126.net/9KeyafHLjadqSQTRS_tN5Q==/5741649720318487.jpg?param=90y90',
       }
   ]
