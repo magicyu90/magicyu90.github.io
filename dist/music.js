e7a91f9194b3d9200b41002a8b3cd3229a5b39ad
magicyu90 <magicyu90@gmail.com>
2019-11-20 21:44:24 +0800

    Site updated: 2019-11-20 21:44:10

--- dist/music.js
+++ dist/music.js
@@ -10,7 +10,7 @@ const ap = new APlayer({
       {
           name: '沙滩恋曲',
           artist: '头文字D 电影原声',
-          url: 'https://win-web-ra01-sycdn.kuwo.cn/c7822a134b1d2d6b338c92c01907e832/5dd17566/resource/n1/128/57/69/3172436728.mp3',
+          url: 'http://39.106.214.208:82/files/music/Tanninginyoursunray.mp3',
           cover: 'https://p2.music.126.net/9KeyafHLjadqSQTRS_tN5Q==/5741649720318487.jpg?param=90y90',
       }
   ]
